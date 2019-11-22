200c6153da4a682441a33fdd8ae76d05b4b4d616
Mijeong <ffan0811@gmail.com>
2019-11-22 11:27:50 +0900

    minimum setting

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const template = <p>Hello from reasct</p>;
+
+ReactDOM.render(template, document.getElementById('root'));
