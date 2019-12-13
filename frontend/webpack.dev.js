e73a228cccf1bb2f97b5f657b36f798e490b7a41
mijeong <ffan0811@naver.com>
2019-12-13 14:00:58 +0900

    added a package, file-loader

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -53,6 +53,16 @@ module.exports = {
                         }
                     }
                 ]
+            }, {
+                test: /\.(png|jpg|gif|svg|mp4|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },
