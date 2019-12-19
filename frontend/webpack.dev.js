a5bfa68314f0bb4f719a800f79bd18a902068025
Mijeong <ffan0811@gmail.com>
2019-12-19 13:43:09 +0900

    made scss and css seperately in webpack config

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,83 +1,85 @@
-const commonPaths = require('./build-utils/common-paths');
+const commonPaths = require("./build-utils/common-paths");
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: `${commonPaths.appEntry}/index.js`
-    },
-    output: {
-        filename: '[name].[hash].js',
-        publicPath: '/',
-        path: commonPaths.outputPath,
-    },
-    resolve: {
-        modules: [
-            commonPaths.appEntry,
-            "node_modules"
-        ]
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(css||scss)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: {
-                                localIdentName: '[path][name]__[local]', 
-                            }
-                        }
-                    }, {
-                        loader: 'postcss-loader'
-                    }, {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            sassOptions: {
-                                includePaths: [
-                                    `${commonPaths.appEntry}/styles`
-                                ]
-                            }
-                        }
-                    }
-                ]
-            }, {
-                test: /\.(png|jpg|gif|svg|mp4|woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    devServer: {
-        port: `${commonPaths.port ? commonPaths.port : 8080}`,
-        historyApiFallback: true,
-        open: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            favicon: 'public/favicon.ico'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/styles.[hash].css'
-        })
-    ]
+	mode: "development",
+	entry: {
+		app: `${commonPaths.appEntry}/index.js`
+	},
+	output: {
+		filename: "[name].[hash].js",
+		publicPath: "/",
+		path: commonPaths.outputPath
+	},
+	resolve: {
+		modules: [commonPaths.appEntry, "node_modules"]
+	},
+	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+							modules: {
+								localIdentName: "[path][name]__[local]"
+							}
+						}
+					},
+					{
+						loader: "postcss-loader"
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+							sassOptions: {
+								includePaths: [`${commonPaths.appEntry}/styles`]
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|jpg|gif|svg|mp4|woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]"
+						}
+					}
+				]
+			}
+		]
+	},
+	devServer: {
+		port: `${commonPaths.port ? commonPaths.port : 8080}`,
+		historyApiFallback: true,
+		open: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "public/index.html",
+			favicon: "public/favicon.ico"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/styles.[hash].css"
+		})
+	]
 };
